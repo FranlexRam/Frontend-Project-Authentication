4727ebe8d7e640a1c1c89e1dceaca180097df6e3
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-08 22:49:21 -0400

    Responsiveness

--- src/Layouts/ProfileLayout/ProfileLayout.js
+++ src/Layouts/ProfileLayout/ProfileLayout.js
@@ -1,16 +1,26 @@
 import Sidebar from "../../components/Sidebar/Sidebar.js";
 import Appbar from "../../components/Appbar/Appbar.js";
 import './profilelayout.css';
+import { useState } from "react";
 
 // const ProfileLayout = ({isLoggedIn,setIsLoggedIn}) => {
     const ProfileLayout = () => {
         // const {isLoggedIn,setIsLoggedIn} = props
+        const [sidebar, setSidebar] = useState(false)
+
+        const handleSidebar = () =>{
+            setSidebar(!sidebar)
+        }
+
+
     return (
         <div className="profilelayout"  >
             {/* appbar */}
-            <Appbar />
+            <Appbar handleSidebar={handleSidebar} />
             {/* sidebar */}
-            <Sidebar />
+            <div className={sidebar ? "profilelayout_sidebar open" : "profilelayout_sidebar"}>
+                <Sidebar />
+            </div>
             {/* content */}
             <div className="profilelayout_content">
                 {/* feed */}
