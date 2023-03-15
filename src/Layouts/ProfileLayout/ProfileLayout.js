94f0e74bb02cddb509fdf7ae355e10875402dc3a
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-03-14 22:03:09 -0400

    Correcting errors

--- src/Layouts/ProfileLayout/ProfileLayout.js
+++ src/Layouts/ProfileLayout/ProfileLayout.js
@@ -1,44 +1,38 @@
-import Profile from "../../components/Profile/Profile.js";
-import Feed from "../../components/Feed/Feed.js";
-import Sidebar from "../../components/Sidebar/Sidebar.js";
-import Appbar from "../../components/Appbar/Appbar.js";
-import './profilelayout.css';
+import Feed from "../../components/Feed/Feed";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Appbar from "../../components/Appbar/Appbar";
+import "./profilelayout.scss";
 import { useState } from "react";
 
+const ProfileLayout = () => {
+  const [sidebar, setSidebar] = useState(false);
 
-// const ProfileLayout = ({isLoggedIn,setIsLoggedIn}) => {
-    const ProfileLayout = () => {
-        // const {isLoggedIn,setIsLoggedIn} = props
-        const [sidebar, setSidebar] = useState(false)
-        
-        /* Funcion que permite al usuario dar click en el icono menu
-         para desplegar el sidebar */
-        const handleSidebar = () =>{
-            setSidebar(!sidebar)
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  return (
+    <div className="profilelayout">
+      {/* appbar */}
+      <Appbar handleSidebar={handleSidebar} />
+      {/* sidebar */}
+      <div
+        className={
+          sidebar ? "profilelayout_sidebar open" : "profilelayout_sidebar"
         }
-
-
-    return (
-        <div className="profilelayout"  >
-            {/* appbar */}
-            <Appbar handleSidebar={handleSidebar} />
-            {/* sidebar */}
-            <div className={sidebar ? "profilelayout_sidebar open" : "profilelayout_sidebar"}>
-                <Sidebar />
-            </div>
-            {/* content */}
-            <div className="profilelayout_content">
-                {/* feed */}
-                <div className="profilelayout_content-feed">
-                    <Feed />
-                </div>
-                {/* profile */}
-                <div className="profilelayout_content-profile">
-                    <Profile />
-                </div>
-            </div>
+      >
+        <Sidebar />
+      </div>
+      {/* content */}
+      <div className="profilelayout_content">
+        {/* feed */}
+        <div className="profilelayout_content-feed">
+          <Feed />
         </div>
-    );
-}
- 
+        {/* profile */}
+      </div>
+    </div>
+  );
+};
+
 export default ProfileLayout;
