01550e2c9297168620a4fb05c219eb9d5b96f481
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-04-13 07:56:20 -0400

    Register 1st part

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -2,9 +2,22 @@ import Input from "../input/input.js";
 import {MdVisibility} from 'react-icons/md'
 import {MdVisibilityOff} from 'react-icons/md'
 import { useState } from "react";
+import {isEmpty, isEmail, isLength, isMatch} from "../helper/validate.js";
+import axios from 'axios';
+import {ToastContainer, toast} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    cf_password: ''
+}
 
 const Register = () => {
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
+    const [data, setData] = useState(initialState);
+    const {name, email, password, cf_password} = data //min 5:55
 
     const handleClick = ()=> {
         setVisible(!visible);
