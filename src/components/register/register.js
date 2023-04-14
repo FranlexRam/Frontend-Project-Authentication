3dfe5a143d90f971dc4ca726150c70c9f5fc0983
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-04-13 22:15:43 -0400

    Register

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -17,26 +17,44 @@ const initialState = {
 const Register = () => {
     const [visible, setVisible] = useState(false);
     const [data, setData] = useState(initialState);
-    const {name, email, password, cf_password} = data //min 5:55
+    const {name, email, password, cf_password} = data
+
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }
 
     const handleClick = ()=> {
         setVisible(!visible);
     };
 
+    const register = async (e) => {
+        e.preventDefault()
+        //check fields
+        if(isEmpty(name) || isEmpty(password))
+        return toast("Please fill in all fields.", {
+            className: 'toast-failed',
+            bodyClassName: //8:21
+        });
+    }
+
     return ( <form>
-        <Input type="text" text="Name"/>
-        <Input type="text" text="Email"/>
+        <Input type="text" text="Name" name="name" handleChange={handleChange}/>
+        <Input type="text" text="Email" name="email" handleChange={handleChange}/>
         <Input
+        name="password"
             type={visible ? "text" : "password"}
             icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
             text="Password"
             handleClick={handleClick}
+            handleChange={handleChange}
         />
         <Input
+        name="cf_password"
             type={visible ? "text" : "password"}
             icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
             text="Confirm Password"
             handleClick={handleClick}
+            handleChange={handleChange}
         />
         <div className="login_btn">
             <button>register</button>
