adb07461eef7e325a0ebb0de2723596d5fb7e75b
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-04-19 17:46:05 -0400

    Sign In 2nd part

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,33 +1,81 @@
-import { useState } from 'react';
-import {FcGoogle} from 'react-icons/fc'
-import {MdVisibility} from 'react-icons/md'
-import {MdVisibilityOff} from 'react-icons/md'
-import Input from "../input/input"
+import { useContext, useState } from 'react';
+import {FcGoogle} from 'react-icons/fc';
+import {MdVisibility} from 'react-icons/md';
+import {MdVisibilityOff} from 'react-icons/md';
+import {isEmpty, isEmail} from "../helper/validate.js";
+import axios from 'axios';
+import {ToastContainer, toast} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import Input from "../input/input";
 import "./login.scss";
+import {AuthContext} from "../../context/AuthContext.js";
+
+const initialState = {
+    name: '',
+    password: ''
+}
 
 const Login = () => {
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
+    const [data, setData] = useState(initialState)
+    const {email, password} = data
+    const {dispatch} = useContext(AuthContext)
+
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value});
+    };
 
     const handleClick = ()=> {
         setVisible(!visible);
     };
 
+    const login = async (e) => {
+        //check fields
+        e.preventDefault()
+        if(isEmpty(email) || isEmpty(password))
+        return toast("Please fill in all fields.", {
+            className: 'toast-failed',
+            bodyClassName: 'toast-failed',
+        })
+        //check email
+        if(!isEmail(email))
+        return toast("Please enter a valid email address.", {
+            className: 'toast-failed',
+            bodyClassName: 'toast-failed',
+        });
+        try {
+            await axios.post('/api/auth/signing', {email, password})
+            localStorage.setItem('_appSignging', true)
+            dispatch({type: "SIGNING"});
+        } catch (err) {
+            toast(err.response.data.msg, {
+                className: 'toast-failed',
+                bodyClassName: 'toast-failed',
+            })
+        }
+    }
+
     return (
-        <form className="login">
-            <Input type="email" text="Email"/>
+        <>
+        <ToastContainer/>
+        <form className="login" onSubmit={login}>
+            <Input type="email" text="Email" name="email" handleChange={handleChange}/>
             <Input
+            name="password"
                 type={visible ? "text" : "password"}
                 icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
                 text="Password"
-                handleClick={handleClick}    
+                handleClick={handleClick}  
+                handleChange={handleChange}
             />
             <div className="login_btn">
-                <button>login</button>
+                <button type='submit'>login</button>
                 <button className="btn-alt">
                     sign in <FcGoogle />
                 </button>
             </div>
         </form>
+        </>
     );
 };
 
